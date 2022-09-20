99e22e6b3d12dfea04580498ca24409b208d5905
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-09-20 11:10:47 -0400

    updates

--- components/Status.js
+++ components/Status.js
@@ -1,35 +1,36 @@
 import * as React from "react";
+import styles from '../styles/navigation.module.css'
 
 
-const statusElement = {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    width: 32,
-    height: 32,
-    background: "rgba(255, 255, 255, 0.6)",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+// const statusElement = {
+//     position: "absolute",
+//     top: 0,
+//     right: 0,
 
-};
+//     height: 32,
+//     background: "rgba(255, 255, 255, 0.6)",
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
 
-const emojiIcon = {
-    width: "1em",
-    height: "1em",
-    lineHeight: 1
-};
+// };
 
-function Status({ inView }) {
+// const emojiIcon = {
+//     width: "1em",
+//     height: "1em",
+//     lineHeight: 1
+// };
+
+function Status({ inView, }) {
     return (
-        <div style={{ position: "sticky", top: 0 }}>
-            <div style={statusElement}>
+        <div className={styles.sticky}>
+            <div className={styles.statusElement}>
                 {inView ? (
-                    <span role="img" aria-label="In view" style={emojiIcon}>
+                    <span role="img" aria-label="In view" className={styles.emojiIcon}>
                         ✅
                     </span>
                 ) : (
-                    <span role="img" aria-label="Outside the viewport" style={emojiIcon}>
+                    <span role="img" aria-label="Outside the viewport" clasName={styles.anotherStyle}>
                         ❌
                     </span>
                 )}
