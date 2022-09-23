4fa04364002c48cc9b6fd9b9b20df081444099a7
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-09-23 15:29:12 -0400

    component update

--- pages/testScrollPositionY.js
+++ pages/testScrollPositionY.js
@@ -1,69 +1,69 @@
-// const Hamburger = {
-//     backgroundColor: "#FFFF00",
-// }
+const Hamburger = {
+    backgroundColor: "#FFFF00",
+}
 
-// const notAHamburger = {
-//     backgroundColor: "#689F38"
-// }
+const notAHamburger = {
+    backgroundColor: "#689F38"
+}
 
-// console.log(Hamburger)
+console.log(Hamburger)
 
-// const ScrollDepth = () => {
-//     offsetHeight > 300 ? { Hamburger } : { notAHamburger }
-// }
+const ScrollDepth = () => {
+    offsetHeight > 300 ? { Hamburger } : { notAHamburger }
+}
 
-// const Scroll = () => {
-//     const handleScroll = event => {
-//         console.log('scrollTop: ', event.currentTarget.scrollTop);
-//         console.log('offsetHeight: ', event.currentTarget.offsetHeight);
-//     };
+const Scroll = () => {
+    const handleScroll = event => {
+        console.log('scrollTop: ', event.currentTarget.scrollTop);
+        console.log('offsetHeight: ', event.currentTarget.offsetHeight);
+    };
 
-//     return (
-//         <div
-//             className={ScrollDepth}
-//             style={{
-//                 border: '3px solid black',
-//                 width: '400px',
-//                 height: '500px',
-//                 overflow: 'scroll',
-//             }}
-//             onScroll={handleScroll}
-//         >
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//             <p>Content</p>
-//         </div>
-//     );
-// };
+    return (
+        <div
+            className={ScrollDepth}
+            style={{
+                border: '3px solid black',
+                width: '400px',
+                height: '500px',
+                overflow: 'scroll',
+            }}
+            onScroll={handleScroll}
+        >
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+        </div>
+    );
+};
 
-// export default Scroll;
+export default Scroll;
